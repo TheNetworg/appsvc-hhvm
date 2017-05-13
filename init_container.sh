d46270e6de4813ceb9cbfcba6c71af7fb355031b
Jan Hajek <hajek.j@hotmail.com>
2017-05-13 10:08:52 +0200

    Update init_container.sh

--- init_container.sh
+++ init_container.sh
@@ -2,3 +2,4 @@
 service ssh start
 touch /home/LogFiles/node_$WEBSITE_ROLE_INSTANCE_ID_out.log
 echo "$(date) Container started" >> /home/LogFiles/node_$WEBSITE_ROLE_INSTANCE_ID_out.log
+/usr/bin/hhvm -m server -c "/etc/hhvm/server.ini" -c "/etc/hhvm/site.ini"
